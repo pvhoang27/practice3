a4edd599424e7f10ff292f94de0e1304c4d6dddd
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2025-11-19 15:50:41 +0700

    update

---
+++ src/ObjectService.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import RMI.ByteService;
+import java.rmi.*;
+import java.rmi.registry.*;
+/**
+ *
+ * @author hoang
+ */
+public class ObjectService {
+    public static void main(String[] args) throws Exception{
+        String studentCode = "B21DCCN633", qCode = "lqBueT9h";
+        Registry rg = LocateRegistry.getRegistry("203.162.10.109");
+        ByteService sv = (ByteService) rg.lookup("RMIByteService");
+        byte [] a = sv.requestData(studentCode, qCode);
+        byte [] kq = new byte[a.length];
+        String kq1 = "";
+        for(int i = 0 ; i < a.length; i++){
+            int n = a[i];
+            String h = Integer.toHexString(n);
+            kq1 += h;
+        }
+        kq = kq1.getBytes();
+        sv.submitData(studentCode, qCode, kq);
+    }
+}
